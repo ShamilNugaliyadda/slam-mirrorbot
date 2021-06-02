5f031ad90614171c01aa2126e49a9b8600bc0df4
ShamilNugaliyadda <64329283+ShamilNugaliyadda@users.noreply.github.com>
2021-06-02 17:38:35 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -257,7 +257,7 @@ except KeyError:
 try:
     IMAGE_URL = getConfig('IMAGE_URL')
 except KeyError:
-    IMAGE_URL = 'https://telegra.ph/file/db03910496f06094f1f7a.jpg'
+    IMAGE_URL = 'https://telegra.ph//file/fec93365851a690a9e454.jpg'
 
 updater = tg.Updater(token=BOT_TOKEN, use_context=True)
 bot = updater.bot
